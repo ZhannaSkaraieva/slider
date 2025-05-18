527af866bda80a9f158e55d8eacb877ee17e44c4
Zhanna Skaraieva <i.m.p.h.e.l.l@gmail.com>
2025-05-18 21:57:54 +0300

    2

--- main.js
+++ main.js
@@ -1,33 +1,57 @@
+const imgArray = ['large_2021.jpg', 'large_2025.jpg', 'large_202500.jpg', 'large_202505.jpg', 'large_202507.jpg'];// массив с изображениями
+
+
 const slide = document.querySelector('.slide');
-    const images = document.querySelectorAll('.slide .image');
-    const leftArrow = document.querySelector('.left');
-    const rightArrow = document.querySelector('.right');
-    const buttons = document.querySelectorAll('.button');
-    const imageWidth = 500;
-    let slideIndex = 0;
+const indicators = document.querySelector('.bottom');
+const leftArrow = document.querySelector('.left');
+const rightArrow = document.querySelector('.right');
+let length = imgArray.length;
 
-    function showSlide(index) {
-      slide.style.transform = `translateX(-${index * imageWidth}px)`;
-      buttons.forEach(btn => btn.style.backgroundColor = 'transparent');
-      buttons[index].style.backgroundColor = 'white';
-    }
+let slideIndex = 1;
+const imageWidth = 500;
+let autoslideInterval;
+let isAutoPlay = false;
 
-    rightArrow.addEventListener('click', () => {
-      slideIndex = (slideIndex + 1) % images.length;
-      showSlide(slideIndex);
+document.addEventListener('DOMContentLoaded', loadImages);
+function loadImages() {
+    for (let i = 0; i < imgArray.length; i++) {
+        const img = document.createElement('img');
+        img.className = 'image';
+        img.src = imgArray[i];
+        slide.appendChild(img);
+    };
+}
+
+for (let i = 0; i < length; i++) {
+    const div = document.createElement('div');
+    div.className = 'buttom';
+    buttons.appendChild(div);
+};
+
+const buttons = document.querySelectorAll('.buttom');
+buttons[0].style.backgroundColor = 'white';
+
+const resetBq = () => {
+    buttons.forEach((button) => {
+        button.style.backgroundColor = "transparent";
     });
+};
 
-    leftArrow.addEventListener('click', () => {
-      slideIndex = (slideIndex - 1 + images.length) % images.length;
-      showSlide(slideIndex);
+
+buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        resetBq();
+        button.style.transform = translateY(-${index * imageWidth}px);
+        slideIndex = index + 1;
+        buttons.slide.backgroundColor = 'white';
     });
+});
+    
+const changeColor = () => {
+    resetBq();
+    buttons[slideIndex - 1].style.backgroundColor = 'white';
+}
 
-    buttons.forEach((button, index) => {
-      button.addEventListener('click', () => {
-        slideIndex = index;
-        showSlide(slideIndex);
-      });
-    });
-
-    // начальная инициализация
-    showSlide(slideIndex);
+const nextSlide = () => {
+    slide.style.transform = translateX(-${(slideIndex - 1) * imageWidth}px);
+}
